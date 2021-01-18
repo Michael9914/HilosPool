ec201b5db47700c4723ec3ad3d3484382c468e0f
Michael9914 <michael_9914@hotmail.com>
2021-01-18 11:31:48 -0500

    Deber Hilos Pool

---
+++ HilosPool/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace HilosPool
+{
+    class Program
+    {
+        const string path = @"D:\Archivos de programa\trabajo";
+        static void Main(string[] args)
+        {
+            for (int i=0; i<=50; i++) 
+            {
+                ThreadPool.QueueUserWorkItem(CrearTxt, i);
+            }
+            while (ThreadPool.PendingWorkItemCount > 0) ;
+
+
+        }
+        static void CrearTxt(object data)
+        {
+            int i = (int)data;
+
+            using (var sw = new StreamWriter(path+i+".txt"))
+            {
+                sw.WriteLine("Buenas, soy el hilo: "+Thread.CurrentThread.ManagedThreadId);
+            }
+            Console.WriteLine("Buenas, soy el hilo: " + Thread.CurrentThread.ManagedThreadId);
+
+        }
+    }
+}
